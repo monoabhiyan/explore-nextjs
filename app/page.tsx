fbc34f1d767d98d9adbde1b0a91656a176295f67
abhiyan <abhiyan.shrestha@monotype.com>
2025-03-25 19:50:16 +0545

    feat: implement prefetching

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import UsersCard from "@/components/users-card";
-import {Suspense} from "react";
+import {dehydrate, HydrationBoundary} from "@tanstack/react-query";
+import {getQueryClient} from "@/app/get-query-client";
+import {usersQueryOptions} from "@/queryOptions/usersQueryOptions";
 
 export default function Home() {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(usersQueryOptions)
   return (
     <div>
-      <Suspense fallback={<div>loading...</div>}>
+      <HydrationBoundary state={dehydrate(queryClient)}>
         <UsersCard/>
-      </Suspense>
+      </HydrationBoundary>
     </div>
   );
 }
