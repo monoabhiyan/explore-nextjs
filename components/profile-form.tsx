e343ca90391aa302899414a41940e49413884e5c
abhiyan <abhiyan.shrestha@monotype.com>
2025-04-05 17:05:25 +0545

    feat: implement next-safe-action using react query

--- components/profile-form.tsx
+++ components/profile-form.tsx
@@ -4,39 +4,70 @@
 'use client';
 
 import {updateUserAction} from "@/app/user/update/user-update.action";
+import {isActionSuccessful, resolveActionResult} from "@/lib/actions";
+import {useMutation} from "@tanstack/react-query";
 
 const ProfileForm = ({user}: { user: never }) => {
+
+  const mutation = useMutation({
+    mutationFn: async (props: { id, email: string; name: string }) => {
+      return resolveActionResult(
+        updateUserAction(props)
+      );
+    },
+    onError: (error) => {
+      alert('use mutation:', error);
+    },
+    onSuccess: (data) => {
+      alert('use mutation: The user has been updated');
+      console.log({
+        data
+      })
+      // router.push('/users');
+    },
+  });
+
   const handleSubmit = async (formData: FormData) => {
     const name = formData.get('name') as string;
     const email = formData.get('email') as string;
 
-    const result = await updateUserAction({
+    // tanstack query:
+    mutation.mutate({
       id: String(user.id),
       name,
       email,
-    })
+    });
 
-    // If result?.data is defined, the action is successful
-    if (result?.data) {
-      alert('The user has been updated');
-      console.log({
-        result,
-      })
-      // router.push('/users');
-      return;
-    }
+    // next-safe-action:
 
-    // If result?.serverError is defined, the action failed
-    if (result?.serverError) {
-      alert(result.serverError ?? 'Error updating user');
-      return;
-    }
+    // const result = await updateUserAction({
+    //   id: String(user.id),
+    //   name,
+    //   email,
+    // })
+    //
+    // // If result?.data is defined, the action is successful
+    // if (result?.data) {
+    //   alert('The user has been updated');
+    //   console.log({
+    //     result,
+    //   })
+    //   // router.push('/users');
+    //   return;
+    // }
+    //
+    // if (!isActionSuccessful(result)) {
+    //   alert(result.serverError ?? 'Error updating user');
+    //   return;
+    // }
+    //
+    // alert('The user has been updated');
   }
 
   return (
     <form action={handleSubmit}>
-      <input style={{ display: 'block' }} name="name" defaultValue={user.name}/>
-      <input style={{ display: 'block' }} name="email" type="email" defaultValue={user.email}/>
+      <input style={{display: 'block'}} name="name" defaultValue={user.name}/>
+      <input style={{display: 'block'}} name="email" type="email" defaultValue={user.email}/>
       <button type="submit">Submit</button>
     </form>
   );
--- lib/actions.ts
+++ lib/actions.ts
@@ -1,9 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
-import {createMiddleware, createSafeActionClient} from 'next-safe-action';
+import {createMiddleware, createSafeActionClient, SafeActionResult} from 'next-safe-action';
 import {getServerSession} from "next-auth";
 import {authOptions} from "@/lib/nextAuth";
-import { ZodError } from "zod";
+import {ZodError} from "zod";
 
 export class ActionError extends Error {
 }
@@ -38,3 +38,52 @@ export const actionClient = createSafeActionClient({
 });
 
 export const authActionClient = actionClient.use(authenticationMiddleware);
+
+/**
+ * Determines if a server action is successful or not
+ * A server action is successful if it has a data property and no serverError property
+ *
+ * @param action Return value of a server action
+ * @returns A boolean indicating if the action is successful
+ */
+export const isActionSuccessful = <T extends z.ZodType>(
+  action?: SafeActionResult<string, T, readonly [], any, any>
+): action is { data: T; serverError: undefined; validationError: undefined } => {
+  if (!action) {
+    return false;
+  }
+
+  if (action.serverError) {
+    return false;
+  }
+
+  if (action.validationErrors) {
+    return false;
+  }
+
+  return true;
+};
+
+/**
+ * Converts an action result to a promise that resolves to false
+ *
+ * @param action Return value of a server action
+ * @returns A promise that resolves to false
+ */
+export const resolveActionResult = async <T extends z.ZodType>(
+  action: Promise<SafeActionResult<string, T, readonly [], any, any> | undefined>
+): Promise<T> => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const result = await action;
+
+      if (isActionSuccessful(result)) {
+        resolve(result.data);
+      } else {
+        reject(new ActionError(result?.serverError ?? 'Something went wrong'));
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
